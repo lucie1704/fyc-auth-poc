e1950c785e5305365e9e2bc79d945e391a0a5120
Jean Pierre Beguerisse <beguerisse@Host-008.lan>
2025-01-07 19:52:32 +0100

    improve authentication with Google OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,18 @@
-import { prisma } from "@/src/lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
+import { prisma } from '@/src/lib/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
